051773e23e493c77105738560d3fd2ca84209ae5
alijr2018 <alijr_2018@proton.me>
2024-03-10 21:26:48 +0000

    3

---
+++ controllers/UsersController.js
@@ -0,0 +1,45 @@
+// UsersController.js
+
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const existingUser = await dbClient.client
+      .db(dbClient.database)
+      .collection('users')
+      .findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = sha1(password);
+
+    const newUser = {
+      email,
+      password: hashedPassword,
+    };
+
+    const result = await dbClient.client
+      .db(dbClient.database)
+      .collection('users')
+      .insertOne(newUser);
+
+    const { insertedId } = result;
+
+    return res.status(201).json({ id: insertedId, email });
+  }
+}
+
+export default UsersController;
