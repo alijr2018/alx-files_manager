edf288c54d76c751dc8a65089afae84615ef69a7
alijr2018 <alijr_2018@proton.me>
2024-03-10 20:34:37 +0000

    set-up

---
+++ utils/redis.js
@@ -0,0 +1,56 @@
+// utils/redis.js
+
+import redis from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    this.client.on('error', (err) => {
+      console.error(`Redis client error: ${err}`);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (error, reply) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (error, reply) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (error, reply) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
